7278e4846f70aa6b306d9bb37ad976ebcf6b5230
KaremReyesA <pq.reyesa@gmail.com>
2025-06-14 00:19:37 -0600

    Update environment.ts

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -10,10 +10,10 @@ export const environment = {
     appId: "1:194670851159:web:bf683b36f7e0c9bd8b5f12"
   },
   //backendUrl: 'http://127.0.0.1:5000'
-  backendUrl: 'http://localhost:5000',
+  backendUrl: 'https://web-production-e4cc.up.railway.app',
   // Nueva URL base para imágenes si las sirves desde un directorio específico en Flask
   // Si Flask sirve los assets desde /assets, usa esta ruta.
   // ¡Asegúrate de que esta URL base coincida con cómo Flask sirve tus archivos estáticos!
-  assetsUrlBase: 'http://localhost:5000/assets'
+  assetsUrlBase: 'https://web-production-e4cc.up.railway.app/assets'
 
 };
